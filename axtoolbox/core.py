1506c0f178f51f0ed62657cd0697ee1513207b0d
John Go <johngo@ThePro.local>
2019-02-12 22:54:36 +0800

    Adding update module

--- axtoolbox/core.py
+++ axtoolbox/core.py
@@ -1,1 +1,26 @@
 # Insert your code here. 
+
+def update():
+	import os
+	os.system('pip install --upgrade git+git://github.com/jcsgo/axtoolbox.git')
+
+
+def setup_encryption(varname:str):
+	encryption_prompt = """
+	Please select encryption type:
+	1 - AES
+	2 - Base64
+	"""
+	encryption_method = input(encryption_prompt)
+
+	key_prompt = """
+	For added security, we require a key of any digit to
+	increase the security of your password
+	"""
+	key = input(key_prompt)
+
+	print('Your encryption method selected is: ', encryption_method)
+	print('Your key is: ', key)
+
+	return None;
+
