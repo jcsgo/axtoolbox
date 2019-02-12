2b6e4d3bcd3bcbc9e33ccedfbb760cb49846429f
John Go <johngo88@gmail.com>
2019-02-12 22:08:35 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+
+"""
+Parts of this file were taken from the pyzmq project
+(https://github.com/zeromq/pyzmq) which have been permitted for use under the
+BSD license. Parts are from lxml (https://github.com/lxml/lxml)
+"""
+
+DISTNAME = 'axtoolbox'
+LICENSE = 'BSD'
+AUTHOR = "John C. S. Go"
+EMAIL = "johngo88@gmail.com"
+URL = "https://github.com/jcsgo/axtoolbox"
+DOWNLOAD_URL = ''
+CLASSIFIERS = [
+    'Development Status :: 1 - Alpha/Unstable',
+    'Environment :: Console',
+    'Operating System :: OS Independent',
+    'Intended Audience :: Business/Analytics',
+    'Programming Language :: Python :: 3.7',
+    'Topic :: Business Intelligence']
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'axtoolbox'
+DESCRIPTION = 'Useful tools for the analytics user for business analytics'
+LONG_DESCRIPTION = """
+**axtoolbox** is a Python package providing useful and safe analytics function
+to jumpstart business analytics function. I will be making and creating
+functions here during my free time and will update this with more descriptions 
+once more functions are uploaded
+"""
+URL = 'https://github.com/jcsgo/axtoolbox'
+EMAIL = 'johngo88@gmail.com'
+AUTHOR = 'John C. S. Go'
+REQUIRES_PYTHON = '>=3.7.0'
+VERSION = None
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    # 'requests', 'maya', 'records',
+]
+
+# What packages are optional?
+EXTRAS = {
+    # 'fancy feature': ['django'],
+}
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+        
+        sys.exit()
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
