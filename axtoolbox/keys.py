8d475cd9cc7ead0bf3ab98adea6941a746a8502c
John Go <johngo@ThePro.local>
2019-02-13 21:26:35 +0800

    update keys.py

--- axtoolbox/keys.py
+++ axtoolbox/keys.py
@@ -10,54 +10,56 @@
 # 2. Find ways to decrypt saved variables (not limited to passwords)
 
 from Crypto.Cipher import AES
+import os
 
 
 def encrypt_text(password):
     obj = AES.new('a$j*$%!GEer9#45%', AES.MODE_CFB, 'o%r_#D&JD^%DHKds')
-    ciphertext = obj.encrypt(password)
+    ciphertext = obj.encrypt(password.encode('utf-8'))
     return ciphertext
 
 
 def decrypt_text(encrypted_password):
     obj2 = AES.new('a$j*$%!GEer9#45%', AES.MODE_CFB, 'o%r_#D&JD^%DHKds')
     password = obj2.decrypt(encrypted_password)
-    return password
+    return password.decode('utf-8')
 
 
-def setup_encryption(varname:str, homedir:str = "~"):
-    value_prompt = """
-    Please enter the plain text to be protected: 
-    """
+def setup_encryption(varname: str):
+    value_prompt = "Please enter your {varname}: ".format(varname=varname)
     ciphertext = encrypt_text(input(value_prompt))
 
     # define path where config file is stored
-    import os
-    os.chdir(os.path.expanduser(homedir))
+    path = os.path.join(os.path.expanduser('~'), 'axtoolbox')
+    if not os.path.exists(path):
+        os.makedirs(path)
+    os.chdir(path)
 
     # defining config name and content
     config_name = "axtoolbox_"+varname+".config"
 
     # writing configuration
-    f = open(config_name,"w+")
-    f.write(str(ciphertext))
+    f = open(config_name, "wb")
+    f.write(ciphertext)
     f.close()
 
     # indicate the file has been written
     print(config_name + ' successfully obscured.')
-    return None;
 
 
-def get_decryption(varname:str, homedir:str = "~"):
+def get_decryption(varname: str):
     # define path where config file is stored
-    import os
-    os.chdir(os.path.expanduser(homedir))
+    path = os.path.join(os.path.expanduser('~'), 'axtoolbox')
+    if not os.path.exists(path):
+        os.makedirs(path)
+    os.chdir(path)
 
     # defining config name and content
     config_name = "axtoolbox_" + varname + ".config"
 
     # writing configuration
-    f = open(config_name, "r")
+    f = open(config_name, "rb")
     line = f.read()
     password = decrypt_text(line)
     f.close()
-    return password;
+    return password
